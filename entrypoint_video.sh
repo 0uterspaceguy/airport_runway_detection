49cae71068cc05b7501bd6515499dd4d53f88bca
krasnoyaruzhskiy <sergey.krasnoyaruzhskiy@gorparkovki.ru>
2024-06-16 06:25:06 +0300

    CHange shapes

--- entrypoint_video.sh
+++ entrypoint_video.sh
@@ -2,11 +2,11 @@ in_video=$1
 out_video=$2
 result_path=$3
 
-frame_w=1920
-frame_h=1080
+# frame_w=1920
+# frame_h=1080
 batch_size=1
 
-muxer='mux.sink_0 nvstreammux name=mux batch-size=1 gpu-id=0 enable-padding=1 live-source=0 batched-push-timeout=40000 width=1920 height=1080'
+muxer='mux.sink_0 nvstreammux name=mux batch-size=1 gpu-id=0 enable-padding=1 live-source=0 batched-push-timeout=40000 width=1024 height=1024'
 primary='nvinfer batch-size=1 config-file-path=./models/primary/config.txt'
 # tracker='nvtracker ll-lib-file=/opt/nvidia/deeepstream/deepstream-6.2/lib/libnvds_nvmultiobjecttracker.so tracker-width=640 tracker-height=384 ll-config-file=/workspace/custom_tracker.yml'
 demuxer='nvstreamdemux name=demux demux.src_0'
